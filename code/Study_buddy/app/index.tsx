b2a297cc7d5ad65bbdd3c16956bf167e10b3b7a7
Kimi <chen.junzhe2000@gmail.com>
2025-10-13 18:01:36 -0400

    Supplementary Information
    Add Framework, manual, AI rate.

--- code/Study_buddy/app/index.tsx
+++ code/Study_buddy/app/index.tsx
@@ -1,3 +1,10 @@
+/*
+  10% framework
+  80% manual
+  10% AI
+*/
+
+
 import { Asset } from "expo-asset";
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, View } from "react-native";
