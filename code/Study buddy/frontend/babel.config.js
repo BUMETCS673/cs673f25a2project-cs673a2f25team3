25b9ec098b0515e6fc41b8ece10b6508a3e65be4
shinushibu1798 <shinu.shibu1798@gmail.com>
2025-10-15 19:17:56 -0400

    Finally figured out the jest errors

--- code/Study buddy/frontend/babel.config.js
+++ code/Study buddy/frontend/babel.config.js
@@ -1,6 +1,9 @@
 // 100% AI generated to ensure frontend tests to work on CI setup
 
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: ['@babel/plugin-transform-flow-strip-types'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['@babel/plugin-transform-flow-strip-types'],
+  };
 };
