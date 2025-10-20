303de13e280b7f5ce402daad75989b222e6db347
IgpayAtenlay <mscron7@gmail.com>
2025-10-20 14:57:19 -0400

    added dead eyes

--- code/Study buddy/frontend/util/buddyEyes.js
+++ code/Study buddy/frontend/util/buddyEyes.js
@@ -1,10 +1,24 @@
-export function buddyEye(status, eyeWidth, startX, middleY, outlineColor, outlineWidth) {
-    switch (status) {
-        case 4: {
-            const yChange = eyeWidth / -2;
-            const xChange = eyeWidth / 4;
-            
-            return <path d={`M ${startX} ${middleY} C ${startX + xChange} ${middleY + yChange}, ${startX + eyeWidth - xChange} ${middleY + yChange}, ${startX + eyeWidth} ${middleY}`} fill="none" stroke={outlineColor} strokeWidth={outlineWidth} />;
-        }
-    }
+export function buddyEye(status, eyeWidth, startX, startY, outlineColor, outlineWidth) {
+	switch (status) {
+		case 4: {
+			const yChange = eyeWidth / -2;
+			const xChange = eyeWidth / 4;
+			
+			return <path d={`M ${startX} ${startY} C ${startX + xChange} ${startY + yChange}, ${startX + eyeWidth - xChange} ${startY + yChange}, ${startX + eyeWidth} ${startY}`} fill="none" stroke={outlineColor} strokeWidth={outlineWidth} />;
+		}
+		case 0: {
+			const eyeHeight = eyeWidth / 2;
+			const left = startX + eyeWidth / 4;
+			const right = startX + eyeWidth * 3 / 4;
+			const top = startY - eyeHeight;
+			const bottom = startY;
+
+			return (
+				<>
+					<path d={`M ${left} ${top} L ${right} ${bottom}`} fill="none" stroke={outlineColor} strokeWidth={outlineWidth} />
+					<path d={`M ${left} ${bottom} L ${right} ${top}`} fill="none" stroke={outlineColor} strokeWidth={outlineWidth} />
+				</>
+			);
+		}
+	}
 }
