8a7352bb2a4c758a4f685b23d992cc2fc21d2db2
shinushibu1798 <shinu.shibu1798@gmail.com>
2025-10-30 23:20:25 -0400

    test(frontend): stub fast refresh globals for jest

--- code/Study buddy/frontend/jest.setup.js
+++ code/Study buddy/frontend/jest.setup.js
@@ -7,6 +7,10 @@ import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/asy
 // Mock AsyncStorage globally
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
+// React Native Fast Refresh stubs (used by React Navigation in tests)
+global.$RefreshReg$ = () => {};
+global.$RefreshSig$ = () => (type) => type;
+
 // Mock expo-linear-gradient
 jest.mock('expo-linear-gradient', () => {
   return {
