bc825b8ec3a7163e1b437c5d9598ef59280515aa
dexzhang-source <dexzhang@bu.edu>
2025-11-06 06:17:48 +0800

    test: fix Jest setup (reanimated & navigation mocks all green)

--- code/Study buddy/frontend/jest.config.js
+++ code/Study buddy/frontend/jest.config.js
@@ -1,40 +1,30 @@
-/** @type {import('jest').Config} */
-const wantStrict = process.env.COVERAGE_80 === '1';
-
+// jest.config.js
 module.exports = {
   preset: 'jest-expo',
-  testEnvironment: 'node',
-  setupFilesAfterEnv: [
-    '<rootDir>/jest.setup.js',
-    '@testing-library/jest-native/extend-expect'
-  ],
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native'
-      + '|@react-native'
-      + '|@react-navigation'
-      + '|react-native-vector-icons'
-      + '|expo'
-      + '|expo-.*'
-      + '|@expo(nent)?/.*'
-      + ')/)',
-  ],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__tests__/__mocks__/fileMock.js',
+    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '^react-native-reanimated$': '<rootDir>/__mocks__/react-native-reanimated.js',
+    '^@env$': '<rootDir>/__mocks__/envMock.js',
   },
-  testPathIgnorePatterns: ['/node_modules/', '/__tests__/__mocks__/'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'components/**/*.{js,jsx,ts,tsx}',
-    'screens/**/*.{js,jsx,ts,tsx}',
-    'util/**/*.{js,jsx,ts,tsx}',
-    'AuthContext.{js,ts,tsx}',
-    '!**/index.{js,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/__tests__/**'
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(' +
+      '(jest-)?react-native' +
+      '|@react-native' +
+      '|react-native-.*' +
+      '|@react-navigation' +
+      '|expo' +
+      '|expo-.*' +
+      '|@expo/.*' +
+      '|react-native-paper' +
+      '|lucide-react-native' +
+    ')/)',
   ],
-  coverageReporters: ['text', 'lcov', 'html'],
-  coverageThreshold: wantStrict
-    ? { global: { branches: 80, functions: 80, lines: 80, statements: 80 } }
-    : { global: { branches: 0, functions: 0, lines: 0, statements: 0 } },
+  clearMocks: true,
+  resetMocks: false,
 };
