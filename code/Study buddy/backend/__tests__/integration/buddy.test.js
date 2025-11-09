1cb662e5ebee31c2cfec3bb097746746288da6ef
IgpayAtenlay <mscron7@gmail.com>
2025-11-09 16:11:52 -0500

    added reset path

--- code/Study buddy/backend/__tests__/integration/buddy.test.js
+++ code/Study buddy/backend/__tests__/integration/buddy.test.js
@@ -82,6 +82,18 @@ describe("Buddy Model Integration Tests", () => {
       done();
     });
   });
+
+  test("Delete Buddy", (done) => {
+    BuddyModel.deleteBuddy(userId, (err) => {
+      expect(err).toBeNull();
+    });
+    
+    BuddyModel.getBuddy(userId, (err, buddy) => {
+      expect(err).toBeNull();
+      expect(buddy).toBeUndefined();
+      done();
+    });
+  });
 });
 
 afterAll(() => db.close());
