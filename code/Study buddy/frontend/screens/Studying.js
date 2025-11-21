f25a1a87eee184081f50ff63cef78e5c96eb3443
IgpayAtenlay <38770334+IgpayAtenlay@users.noreply.github.com>
2025-11-21 15:39:41 -0500

    updated styles import to match other styles imports

--- code/Study buddy/frontend/screens/Studying.js
+++ code/Study buddy/frontend/screens/Studying.js
@@ -8,24 +8,24 @@ import StudyTimerInterface from "../components/StudyTimerInterface";
 import { Background } from "../components/Background";
 import AppIcon from "../components/icons/AppIcon";
 import { iconWrapper } from "../styles/iconStyles";
-import styles from "../styles/studyingStyles";   
+import {studyingStyles} from "../styles/studyingStyles";
 
 export default function Studying() {
   return (
     <Background>
-      <View style={styles.container}>
-        <View style={styles.header}>
+      <View style={studyingStyles.container}>
+        <View style={studyingStyles.header}>
           <View style={iconWrapper}>
             <AppIcon name="clock" size={44} />
           </View>
 
-          <Text style={styles.headerTitle}>Focus Mode</Text>
-          <Text style={styles.motto}>
+          <Text style={studyingStyles.headerTitle}>Focus Mode</Text>
+          <Text style={studyingStyles.motto}>
             Consistency turns effort into progress
           </Text>
         </View>
 
-        <View style={styles.timerWrapper}>
+        <View style={studyingStyles.timerWrapper}>
           <StudyTimerInterface />
         </View>
       </View>
--- code/Study buddy/frontend/styles/studyingStyles.js
+++ code/Study buddy/frontend/styles/studyingStyles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({
+export const studyingStyles = StyleSheet.create({
   iconSpacing: {
     marginRight: 6,
   },
